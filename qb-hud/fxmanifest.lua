40b4a4dea9970687809e081b589d9192823e36c1
LOUAI <121733140+louairgh@users.noreply.github.com>
2024-07-20 17:55:45 +0100

    Add files via upload

---
+++ qb-hud/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+description 'qb-hud'
+version '2.2.0'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua'
+}
+
+client_script { 
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/EntityZone.lua',
+	'@PolyZone/CircleZone.lua',
+	'@PolyZone/ComboZone.lua',  
+	'client.lua',
+    'HRSGears.lua' 
+}
+
+server_script 'server.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/*',
+    'html/index.html',
+    'html/styles.css',
+    'html/responsive.css',
+    'html/app.js',
+}
